47ea6e717a8a691bda3c8bd2f23334c7abd226fe
gnb <gnb@rtbio.ii.uam.es>
2017-09-20 15:49:53 +0200

    Separando autocal 2 parte 2

--- src/calibrate_functions_phase2_a.c
+++ src/calibrate_functions_phase2_a.c
@@ -13,11 +13,12 @@ int auto_calibration(
 					){
 
 	if(args->calibration==1 || args->calibration==2 || args->calibration==3){
-		//Electrica en fase - ecm
+		
+        //Electrica en fase - ecm
 		int ret_ecm = calc_ecm(args->vars[0] * cs->scale_virtual_to_real + cs->offset_virtual_to_real, ret_values[0], rafaga_viva_pts, ecm_result);
         msg->ecm = *ecm_result;
 
-        if(cal_on && ret_ecm==1){
+        if(*cal_on && ret_ecm==1){
         	int is_syn;
             if (args->calibration == 1){
                 //Porcentaje
@@ -31,16 +32,14 @@ int auto_calibration(
             }
 
             if (is_syn==TRUE){
-                //printf("CALIBRATION END: g=%f\n", g_virtual_to_real[0]);
-                cal_on=FALSE;
-            }else if(is_syn==FALSE && cal_on==TRUE){
+                printf("CALIBRATION END: g=%f\n", g_virtual_to_real[0]);
+                *cal_on=FALSE;
+            }else if(is_syn==FALSE && *cal_on==TRUE){
+                //printf("%f\n", g_virtual_to_real[0]);
                 change_g(&g_virtual_to_real[0]);
                 change_g(&g_real_to_virtual[0]);
             }
         }
 
-	}
-	
-
-
+	}	
 }
--- src/rt_thread_functions.c
+++ src/rt_thread_functions.c
@@ -432,38 +432,7 @@ void * rt_thread(void * arg) {
             write_comedi(session, args->n_out_chan, args->out_channels, out_values);
 
             /*CALIBRACION*/
-            auto_calibration(args, cal_struct, &ret_values, rafaga_viva_pts, &ecm_result, &msg, &cal_on, g_virtual_to_real, g_real_to_virtual);
-
-/*********
-            if(args->calibration==1 || args->calibration==2 || args->calibration==3){
-                //Electrica en fase - ecm
-                int ret_ecm = calc_ecm(args->vars[0] * scale_virtual_to_real + offset_virtual_to_real, ret_values[0], rafaga_viva_pts, &ecm_result);
-                msg.ecm = ecm_result;
-                if(cal_on && ret_ecm==1){
-
-                    int is_syn;
-                    if (args->calibration == 1){
-                        //Porcentaje
-                        is_syn = is_syn_by_percentage(ecm_result);
-                    }else if (args->calibration == 2){
-                        //Pendiente
-                        is_syn = is_syn_by_slope(ecm_result);
-                    }else if (args->calibration == 3){
-                        //Var
-                        is_syn = is_syn_by_variance(ecm_result);
-                    }
-
-                    if (is_syn==TRUE){
-                        //printf("CALIBRATION END: g=%f\n", g_virtual_to_real[0]);
-                        cal_on=FALSE;
-                    }else if(is_syn==FALSE && cal_on==TRUE){
-                        change_g(&g_virtual_to_real[0]);
-                        change_g(&g_real_to_virtual[0]);
-                    }
-
-                }
-            }
-*////////////
+            auto_calibration(args, cal_struct, ret_values, rafaga_viva_pts, &ecm_result, &msg, &cal_on, g_virtual_to_real, g_real_to_virtual);
 
 
             if (args->calibration==4){
